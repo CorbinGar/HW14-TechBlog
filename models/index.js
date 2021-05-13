56e9d9ee4ff89770ddda2bbd6143e196f6fc038b
Corbin Garner <Corbingar@gmail.com>
2021-05-12 17:59:06 -0700

    added new dependencies

--- models/index.js
+++ models/index.js
@@ -1,5 +1,15 @@
-const User = require('./User')
-const Post = require('./Post')
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+const passport = require('./utils/passport');
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 // User.belongsToMany(Post, {
 //     through: {
