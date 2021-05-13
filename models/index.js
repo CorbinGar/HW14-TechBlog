9a05d960664dd3e051b0cccfbb30861479503336
Corbin Garner <Corbingar@gmail.com>
2021-05-12 17:50:33 -0700

    initial commit, took code from previous project

---
+++ models/index.js
@@ -0,0 +1,38 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
+
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
+
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: 'tags'
+});
+
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: 'products'
+});
+
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
